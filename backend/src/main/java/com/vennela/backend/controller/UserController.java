62baec7177c9c17c5ecfbcdd217002e0bfaddeb6
Venneladora <vennidora2@gmail.com>
2026-06-23 12:31:33 +0530

    Added CORS

--- backend/src/main/java/com/vennela/backend/controller/UserController.java
+++ backend/src/main/java/com/vennela/backend/controller/UserController.java
@@ -6,7 +6,7 @@ import com.vennela.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://vennela-mart.vercel.app")
 
 public class UserController {
 
